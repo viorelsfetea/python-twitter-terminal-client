7cf9bc1064a4d4bb7ef4f033c0bbf2fa6a3fbf96
v.sfetea <v.sfetea@traviangames.com>
2015-10-02 16:18:50 +0200

    Initial commit

---
+++ fabfile.py
@@ -0,0 +1,5 @@
+from fabric.api import local
+
+
+def test():
+    local('nosetests --with-coverage --rednose')
---
+++ setup.py
@@ -0,0 +1,19 @@
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'Twitter terminal client',
+    'author': 'Viorel Sfetea',
+    'url': 'http://viorel.sfetea.ro',
+    'download_url': 'https://github.com/viorelsfetea/python-twitter-terminal-client.git',
+    'author_email': 'viorel.sfetea@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': [],
+    'scripts': [],
+    'name': 'twitter-terminal-client'
+}
+
+setup(**config)
