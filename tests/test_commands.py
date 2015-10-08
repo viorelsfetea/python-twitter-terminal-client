0cd05656929bcdcf0cfff4010be1d87d35a362a0
v.sfetea <v.sfetea@traviangames.com>
2015-10-08 20:57:27 +0200

    Finished commands structure Added separate twitter handler Updated imports to be absolute

---
+++ tests/test_commands.py
@@ -0,0 +1,50 @@
+import unittest
+import os
+from nose.tools import raises
+from mock import patch, MagicMock, PropertyMock
+
+from twitterclient.commands.CommandExit import CommandExit
+from twitterclient.commands.CommandLogout import CommandLogout
+
+
+class View:
+    def update_status_area(self):
+        pass
+
+
+class CommandsTest(unittest.TestCase):
+
+    def setUp(self):
+        self.view = View()
+        self.view.update_status_area = MagicMock(name='update_status_area')
+        self.twitter_auth = None
+
+        self.test_user_config = os.path.join(os.path.dirname(__file__), '.test_user_config')
+
+    @raises(SystemExit)
+    def test_exit(self):
+        CommandExit(self.view, self.twitter_auth).run()
+
+    def test_logout_config_file_exists(self):
+        with patch('twitterclient.helpers.Config.user_config_file', new_callable=PropertyMock) as user_config_file:
+            user_config_file.return_value = self.test_user_config
+
+            #create the config file
+            open(self.test_user_config, 'a').close()
+
+            CommandLogout(self.view, self.twitter_auth).run()
+            self.view.update_status_area.assert_called_once_with(
+                "Successfully logged you out"
+            )
+
+    def test_logout_config_file_missing(self):
+        CommandLogout(self.view, self.twitter_auth).run()
+        self.view.update_status_area.assert_called_once_with(
+            "Could not log you out. Please try again"
+        )
+
+    def tearDown(self):
+        try:
+            os.remove(self.test_user_config)
+        except OSError:
+            pass
